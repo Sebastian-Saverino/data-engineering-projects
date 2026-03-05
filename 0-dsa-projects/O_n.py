8bead8b41b2d95f63d8a56d3fc26b6c2c36696e0
Sebastian Saverino <sebastian.saverino4@gmail.com>
2026-03-04 22:06:26 -0800

    create dsa project folder

---
+++ 0-dsa-projects/O_n.py
@@ -0,0 +1,10 @@
+def sum_list(numbers):
+    total = 0
+    
+    for num in numbers:
+        total += num
+        
+    return total
+
+
+# we iterate through each variable making this constant based on our data 

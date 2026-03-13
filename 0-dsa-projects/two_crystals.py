b55d0b31ec43af45b11b30d681bc454d2962fb2b
Sebastian Saverino <sebastian.saverino4@gmail.com>
2026-03-12 22:57:15 -0700

    Create crystal ball project in pyhton

---
+++ 0-dsa-projects/two_crystals.py
@@ -0,0 +1,32 @@
+import math
+
+data = [True,True,True,True,True,True,False,False,False,False]
+
+jump_amount = int(math.sqrt(len(data)))
+
+while jump_amount == True:
+    data[jump_amount]
+    if data[jump_amount] == False:
+        False
+        for i in data:
+            print(i)
+
+
+
+
+
+
+
+index = 0
+
+# Jump phase
+while index < len(data) and data[index] == True:
+    index += jump
+
+# Linear search phase
+start = index - jump
+
+for i in range(start, min(index + 1, len(data))):
+    if data[i] == False:
+        print("First False at index:", i)
+        break
